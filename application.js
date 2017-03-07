bb69ed272a2fccc501ef4ec8dade108dd7a2a247
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-03-07 09:27:19 -0500

    NorthSide 1.2  /application.js

--- application.js
+++ application.js
@@ -338,7 +338,7 @@ function renderPromotions(container, template, collection){
             var store_details = getStoreDetailsByID(val.promotionable_id);
             val.store_detail_btn = store_details.slug ;
             val.store_name = store_details.name;
-            // val.image_url = store_details.store_front_url_abs;
+            val.image_url = store_details.store_front_url_abs;
         }
         else{
             val.store_name = "Dixie Outlet";
