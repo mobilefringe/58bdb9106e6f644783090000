adafc426485cd91481bfe61094893fda623058c1
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-03-08 10:46:18 -0500

    NorthSide 1.2  /application.js

--- application.js
+++ application.js
@@ -409,7 +409,17 @@ function renderPromoDetails(container, template, collection){
     $(container).html(item_rendered.join(''));
 }
 
+function renderSimple(container, template, collection){
+    var item_list = [];
+    var item_rendered = [];
+    var template_html = $(template).html();
+    Mustache.parse(template_html); 
+    
+    var repo_rendered = Mustache.render(template_html, collection);
+    item_rendered.push(repo_rendered);
 
+    $(container).html(item_rendered.join(''));
+}
 
 function renderStoreDetailsHours(container, template, collection){
     var item_list = [];
