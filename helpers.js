b3e729f5967e2b8f309ab472daf8c8949b02e205
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-03-13 16:27:31 -0400

    NorthSide 1.2  /helpers.js

--- helpers.js
+++ helpers.js
@@ -1,5 +1,5 @@
 function init(e){
-    $('<div class="modal-backdrop custom_backdrop"><img src="//kodekloud.s3.amazonaws.com/sites/554a79236e6f64713f000000/69e8cd982124dc73de1f5a67a627ee75/loading.gif" class="" alt=""></div>').appendTo(document.body);
+    $('<div class="modal-backdrop custom_backdrop"><div class="loader">Loading...</div></div>').appendTo(document.body);
     
     $('.menu_toggler').click(function(){
         $('#navbar').slideToggle();
