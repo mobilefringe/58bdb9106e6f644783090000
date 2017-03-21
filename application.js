d8d2550475f1259c1834f7afc8fc8094e24f3c70
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-03-21 15:11:25 -0400

    NorthSide 1.2  /application.js

--- application.js
+++ application.js
@@ -333,8 +333,8 @@ function renderPromotions(container, template, collection){
             val.image_url  = store_details.store_front_url_abs;
         }
 
-        if (val.name.length > 32){
-            val.name_short = val.name.substring(0,32) + "...";
+        if (val.name.length > 30){
+            val.name_short = val.name.substring(0,30) + "...";
         }
         else {
             val.name_short = val.name;
