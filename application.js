c9381a712b0000881e27bf2d6e26cb2e01f78978
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-03-21 11:54:35 -0400

    NorthSide 1.2  /application.js

--- application.js
+++ application.js
@@ -86,9 +86,10 @@ function renderStoreList(container, template, collection, type){
     $.each(collection, function(key, val) {
         if(type == "stores" || type == "category_stores"){
             if(!val.store_front_url ||  val.store_front_url.indexOf('missing.png') > -1 || val.store_front_url.length === 0){
-                val.alt_store_front_url = default_image.image_url;
-            // } else {
-            //     val.alt_store_front_url = getImageURL(val.store_front_url);    
+                val.store_front_url = val.alt_store_front_url;
+            }
+            if(!val.alt_store_front_url ||  val.alt_store_front_url.indexOf('missing.png') > -1 || val.alt_store_front_url.length === 0){
+                val.alt_store_front_url = default_image.image_url;;    
             }
         }
         
