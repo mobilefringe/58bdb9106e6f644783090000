6c1657479b64c71c25e5472e3031c8d12c76e3af
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-03-21 15:25:48 -0400

    NorthSide 1.2  /helpers.js

--- helpers.js
+++ helpers.js
@@ -287,7 +287,7 @@ function load_store_map(reg, store_details){
         cursor:'pointer',
         responsive:true,
         zoomLimit: [0,5],
-        viewBox:[420,420,1650,1650]
+        // viewBox:[420,420,1650,1650]
     });
     map.setViewBox(store_details.svgmap_region);
     map.selectRegion(store_details.svgmap_region);
