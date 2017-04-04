74c4f47f24212a34f9062825b529563c1c9ccd43
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-04-04 10:34:07 -0400

    NorthSide 1.2  /application.js

--- application.js
+++ application.js
@@ -476,6 +476,19 @@ function renderEvents(container, template, collection){
             var store_details = getStoreDetailsByID(val.eventable_id);
             val.store_detail_btn = store_details.slug ;
             val.store_name = store_details.name;
+            
+            var store_front_image = getStoreDetailsBySlug(val.store_detail_btn).gallery;
+            var store_logo = getStoreDetailsBySlug(val.store_detail_btn).store_front_url_abs;
+            
+            if(store_front_image != undefined) {
+                val.image_url = "//mallmaverick.com" + store_front_image;
+            }
+            if(store_front_image === undefined){
+                val.image_url = store_logo;
+            }
+            if(store_logo.indexOf('missing.png') > 0){
+                val.image_url  = default_image.image_url;
+            }
         }
         else{
             val.store_name = mall_name;
