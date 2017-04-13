4689ef0a2825e4d10228668d0dca165f4cfbe00d
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-04-13 12:10:25 -0400

    NorthSide 1.2  /helpers.js

--- helpers.js
+++ helpers.js
@@ -31,27 +31,27 @@ function init(e){
     
     blog_searcher();
     
-    $('#subForm').submit(function(e){
-        e.preventDefault();
-        if($("#agree_newsletter").is(':checked')){
-            $.getJSON(
-                this.action + "?callback=?",
-                $(this).serialize(),
-                function (data) {
-                    if (data.Status === 400) {
-                        alert("An error occured while processing your request. Please try again later.");
-                    } else { // 200
-                        $("#subscription_confirmed").fadeIn();
-                        $('#subscription_confirmed').delay(2000).fadeOut();
-                        $('#subForm').trigger('reset');
-                    }
-            });
-        }
-        else{
-            $("#agree_newsletter").focus();
-            alert("Please agree to receive newsletter before continuing.")
-        }
-    });
+    // $('#subForm').submit(function(e){
+    //     e.preventDefault();
+    //     if($("#agree_newsletter").is(':checked')){
+    //         $.getJSON(
+    //             this.action + "?callback=?",
+    //             $(this).serialize(),
+    //             function (data) {
+    //                 if (data.Status === 400) {
+    //                     alert("An error occured while processing your request. Please try again later.");
+    //                 } else { // 200
+    //                     $("#subscription_confirmed").fadeIn();
+    //                     $('#subscription_confirmed').delay(2000).fadeOut();
+    //                     $('#subForm').trigger('reset');
+    //                 }
+    //         });
+    //     }
+    //     else{
+    //         $("#agree_newsletter").focus();
+    //         alert("Please agree to receive newsletter before continuing.")
+    //     }
+    // });
 }
 
 function render_instagram(data){
