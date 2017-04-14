8e74151c55c731c8102bedd2120bc70f8c32805f
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-04-13 21:07:03 -0400

    NorthSide 1.2  /application.js

--- application.js
+++ application.js
@@ -496,10 +496,6 @@ function renderEvents(container, template, collection){
         else{
             val.store_name = "Domain NORTHSIDE";
             val.image_url = val.event_image_url_abs;
-            
-            // if(val.event_image_url_abs === undefined || val.event_image_url_abs.indexOf('missing.png') < 0){
-            //     val.image_url = default_image.image_url;
-            // }
         }
         
         if(val.event_image_url_abs.indexOf('missing.png') < 0){
@@ -509,7 +505,6 @@ function renderEvents(container, template, collection){
                 val.image_url = default_image.image_url;
             }
         }
-        
         
         if (val.name.length > 30){
             val.name_short = val.name.substring(0,30) + "...";
