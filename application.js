04801da812645741b5dabdaa035195649d08dfd5
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-07-11 16:18:35 -0400

    NORTHSIDE 1.3  /application.js

--- application.js
+++ application.js
@@ -329,22 +329,23 @@ function renderPromotions(container, template, collection){
         }
         
         var store_details = getStoreDetailsByID(val.promotionable_id);
-        console.log(store_details)
-        
-        // var store_front = getStoreDetailsBySlug(val.store_detail_btn);
-        
-        var store_front_image = store_details.gallery;
-        var store_logo = store_details.store_front_url_abs;
-        
-        if(store_front_image != undefined) {
-            val.image_url = "//mallmaverick.com" + store_front_image;
-        }
-        if(store_front_image === undefined){
+        if(store_logo != undefined){
             val.image_url = store_logo;
-        }
-        if(store_logo.indexOf('missing.png') > 0 || store_logo == undefined){
+        } else {
             val.image_url  = default_image.image_url;
         }
+        // var store_front_image = getStoreDetailsBySlug(val.store_detail_btn).gallery;
+        // var store_logo = getStoreDetailsBySlug(val.store_detail_btn).store_front_url_abs;
+        
+        // if(store_front_image != undefined) {
+        //     val.image_url = "//mallmaverick.com" + store_front_image;
+        // }
+        // if(store_front_image === undefined){
+        //     val.image_url = store_logo;
+        // }
+        // if(store_logo.indexOf('missing.png') > 0){
+        //     val.image_url  = default_image.image_url;
+        // }
 
         if (val.name.length > 32){
             val.name_short = val.name.substring(0,30) + "...";
@@ -353,8 +354,8 @@ function renderPromotions(container, template, collection){
             val.name_short = val.name;
         }
         
-        // var store_categories = getStoreDetailsByID(val.promotionable_id).categories;
-        // val.cat_list = store_categories.join(',');
+        var store_categories = getStoreDetailsByID(val.promotionable_id).categories;
+        val.cat_list = store_categories.join(',');
         
         var show_date = moment(val.show_on_web_date);
         var start = moment(val.start_date).tz(getPropertyTimeZone());
