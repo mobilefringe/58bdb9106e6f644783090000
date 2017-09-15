2c7a60df1a7d27cb0929e9ddf9f01d72b08a177e
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-09-15 16:55:39 -0400

    NORTHSIDE 1.4  /helpers.js

--- helpers.js
+++ helpers.js
@@ -492,7 +492,6 @@ function getAssetURL(id){
     var store_assets = "https://northside.mallmaverick.com/api/v4/northside/stores/" + store_id + "/store_files.json"
     $.getJSON(store_assets).done(function(data) {
         var asset_url =  "https://www.mallmaverick.com" + data.store_files[0].url;
-        console.log(asset_url)
         return asset_url;
     }).fail(function(jqXHR) {
         if (jqXHR.status == 404) {
