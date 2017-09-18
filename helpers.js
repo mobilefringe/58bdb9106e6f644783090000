91b7bd1224d3997c7be1b827be430b7ce58e51ef
rajbir@mobilefringe.com @ CodeCloud <rajbir@mobilefringe.com @ CodeCloud>
2017-09-18 16:28:56 -0400

    NORTHSIDE 1.4  /helpers.js

--- helpers.js
+++ helpers.js
@@ -498,9 +498,7 @@ function getAssetURL(id){
         success: function(data) {
             if(data.store_files.length > 0){
                 store_front_image_url =  "https://www.mallmaverick.com" + data.store_files[0].url;
-            }else{
-                store_front_image_url = undefined; 
-                }
+            }
             return store_front_image_url
         },
         error: function (data){
