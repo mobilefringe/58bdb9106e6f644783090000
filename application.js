9027cbfd5be68d8c94458d00ec05c104ddda74cc
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-09-18 12:36:56 -0400

    NORTHSIDE 1.4  /application.js

--- application.js
+++ application.js
@@ -327,6 +327,7 @@ function renderPromotions(container, template, collection){
                                 store_front_image_url =  "https://www.mallmaverick.com" + data.store_files[0].url;
                                 return store_front_image_url;
                                 console.log(store_front_image_url)
+                            }
                         });
                             
                         // });
