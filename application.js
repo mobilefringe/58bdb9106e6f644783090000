7cdbbd38093482b13341689d5f0d39b825c059e1
rajbir@mobilefringe.com @ CodeCloud <rajbir@mobilefringe.com @ CodeCloud>
2017-09-18 16:26:15 -0400

    NORTHSIDE 1.4  /application.js

--- application.js
+++ application.js
@@ -315,7 +315,7 @@ function renderPromotions(container, template, collection){
                 var store_details = getStoreDetailsByID(val.promotionable_id);
                 val.store_detail_btn = store_details.slug ;
                 val.store_name = store_details.name;
-                var store_front_image = getAssetURL(val.id);
+                var store_front_image = getAssetURL(val.promotionable_id);
                 var store_logo = getStoreDetailsBySlug(val.store_detail_btn).store_front_url_abs;
                 
                 if(store_front_image != undefined) {
