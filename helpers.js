a69e10cebe1f879459e3387c0b83e2b5a0d0a144
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-12-12 11:52:14 -0500

    NORTHSIDE 1.4  /helpers.js

--- helpers.js
+++ helpers.js
@@ -53,8 +53,8 @@ function init(e){
     //     }
     // });
     //dynamically changing copyright year
-var current_year = moment().year();
-$("#current_year").text(current_year);
+    var current_year = moment().year();
+    $("#current_year").text(current_year);
 }
 
 function render_instagram(data){
