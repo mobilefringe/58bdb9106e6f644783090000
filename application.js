57002e9dbca5950361d57f3af57fd7d7261c01d6
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-02-14 17:02:46 -0500

    NORTHSIDE 1.4  /application.js

--- application.js
+++ application.js
@@ -321,14 +321,18 @@ function renderPromotions(container, template, collection){
                 var store_front_image = getAssetURL(val.promotionable_id);
                 var store_logo = getStoreDetailsBySlug(val.store_detail_btn).store_front_url_abs;
                 
-                if(store_front_image !== undefined) {
-                    val.image_url = "//mallmaverick.com" + store_front_image;
-                }
-                if(store_front_image === ""){
-                    val.image_url = store_logo;
-                }
-                if(store_logo.indexOf('missing.png') > 0){
-                    val.image_url  = default_image.image_url;
+                if(val.is_special_promo == true){
+                    val.image_url = val.promo_image_url_abs    
+                } else {
+                    if(store_front_image !== undefined) {
+                        val.image_url = "//mallmaverick.com" + store_front_image;
+                    }
+                    if(store_front_image === ""){
+                        val.image_url = store_logo;
+                    }
+                    if(store_logo.indexOf('missing.png') > 0){
+                        val.image_url  = default_image.image_url;
+                    }
                 }
 
                 var store_categories = getStoreDetailsByID(val.promotionable_id).categories;
