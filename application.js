4257d37303a107b5f50042439b69ee6e7d5e24e7
irene@mobilefringe.com @ CodeCloud <irene@mobilefringe.com @ CodeCloud>
2020-03-27 12:00:21 -0400

    NORTHSIDE 1.5  /application.js

--- application.js
+++ application.js
@@ -606,7 +606,7 @@ function renderEvents(container, template, collection){
         }
         
         if (val.name.length > 30){
-            val.name_short = val.name.substring(0,30) + "...";
+            val.name_short = val.name.substring(0,40) + "...";
         } else {
             val.name_short = val.name;
         }
