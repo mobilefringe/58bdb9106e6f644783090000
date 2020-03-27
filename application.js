b634f0c0fa49428f01f593be74ac89e821890fa9
irene@mobilefringe.com @ CodeCloud <irene@mobilefringe.com @ CodeCloud>
2020-03-27 12:38:30 -0400

    NORTHSIDE 1.5  /application.js

--- application.js
+++ application.js
@@ -609,7 +609,7 @@ function renderEvents(container, template, collection){
         if (val.name.length > 30){
             // val.name_short = val.name.substring(0,45)
             val.name = 'fives fives fives fives fives fives fives fives fives'
-            val.name_short - val.name.substring(0, 45)
+            val.name_short = val.name.substring(0, 45)
             if (val.name_short.length > 45) {
                 val.name_short = val.name_short + "...";
             } else {
