3e4eb4de3dfc8c75757b5df3f4cf2b4b5e9b848a
irene@mobilefringe.com @ CodeCloud <irene@mobilefringe.com @ CodeCloud>
2020-03-27 12:34:19 -0400

    NORTHSIDE 1.5  /application.js

--- application.js
+++ application.js
@@ -378,7 +378,7 @@ function renderPromotions(container, template, collection){
             }
             
             if (val.name.length > 32){
-                val.name_short = val.name.substring(0,50) + "...";
+                val.name_short = val.name.substring(0,40) + "...";
             } else {
                 val.name_short = val.name;
             }
@@ -605,15 +605,8 @@ function renderEvents(container, template, collection){
             }
         }
         
-        if (val.name.length > 40){
-            // val.name_short = val.name.substring(0,45)
-            val.name="fives fives fives fives fives fives fives fives"
-            // console.log(val.name.length)
-            val.name_short = val.name.substring(0,40)
-            if (val.name_short.length > 40) {
-                val.name_short = val.name_short + "..."
-                console.log(val.name_short)
-            } 
+        if (val.name.length > 30){
+            val.name_short = val.name.substring(0,40) + "...";
         } else {
             val.name_short = val.name;
         }
